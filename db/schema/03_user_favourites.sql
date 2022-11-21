6f1c6db0c9594fcc19b1bf0989834055f5f1ece7
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-21 11:41:30 -0500

    Bug fix error user_favourites were creating user_contributions table. Fixed it back to user_favourites

--- db/schema/03_user_favourites.sql
+++ db/schema/03_user_favourites.sql
@@ -1,8 +1,8 @@
 -- Drop and recreate Users table (Example)
 
-DROP TABLE IF EXISTS user_contributions CASCADE;
-CREATE TABLE user_contributions (
+DROP TABLE IF EXISTS user_favourites CASCADE;
+CREATE TABLE user_favourites (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) NOT NULL
+  maps_id INTEGER REFERENCES maps(id) NOT NULL
 );
- 
