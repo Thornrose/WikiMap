3daa2b8f54625a4b418b51a5d7bfb1132c045091
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-21 11:56:49 -0500

    Fix table name typo the table was originally named user_contributions; fixed to be user_favourites

--- db/schema/03_user_favourites.sql
+++ db/schema/03_user_favourites.sql
@@ -1,7 +1,7 @@
 -- Drop and recreate Users table (Example)
 
-DROP TABLE IF EXISTS user_contributions CASCADE;
-CREATE TABLE user_contributions (
+DROP TABLE IF EXISTS user_favourites CASCADE;
+CREATE TABLE user_favourites (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) NOT NULL
 );
