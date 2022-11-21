53daa01832938d558825a116cb50e2d0763e0390
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-21 18:57:50 -0500

    Add input form and buttons added input forms for point title, description, and image url; added buttons; Created a new map.js and point.js file to reference the map function in index.ejs

---
+++ public/scripts/point.js
@@ -0,0 +1,19 @@
+var popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent(`
+      <div class='popup'>
+        <form class='new_point_form'>
+          <input name='point_title' type='text' placeholder='point title'></input>
+          <input name='point_description' type='text' placeholder='point description'></input>
+          <input name='point_image_url' type='text' placeholder='point image url'></input>
+          <button type='submit' action='post'>ADD</button>
+          </form>
+        </div>
+      `)
+    .openOn(map);
+}
+
+map.on("click", onMapClick);
