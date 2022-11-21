1b5de7512a3dfdb409ffa2da97b762a8337293ba
Cheever Esler <ames.esler@gmail.com>
2022-11-21 13:29:16 -0400

    fix missing commas in two db sql files missing commas in maps and user_favourites tables

--- db/schema/03_user_favourites.sql
+++ db/schema/03_user_favourites.sql
@@ -3,6 +3,6 @@
 DROP TABLE IF EXISTS user_favourites CASCADE;
 CREATE TABLE user_favourites (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) NOT NULL
+  user_id INTEGER REFERENCES users(id) NOT NULL,
   maps_id INTEGER REFERENCES maps(id) NOT NULL
 );
