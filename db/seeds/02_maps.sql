7ae940a97d85d31fa25c62d97cd0634e00361f23
Cheever Esler <ames.esler@gmail.com>
2022-11-22 00:14:03 -0400

    add and update seed files for all tables

---
+++ db/seeds/02_maps.sql
@@ -0,0 +1,6 @@
+-- Maps table seeds
+
+INSERT INTO maps (owner_id, name) VALUES (1, 'Best Places');
+INSERT INTO maps (owner_id, name) VALUES (1, 'Worst Places');
+INSERT INTO maps (owner_id, name) VALUES (2, 'Places I have been');
+INSERT INTO maps (owner_id, name) VALUES (2, 'Places I want to go');
---
+++ db/seeds/05_user_favourites.sql
@@ -0,0 +1,12 @@
+-- User favourites seeds
+
+INSERT INTO user_favourites (user_id, map_id) VALUES (1, 1);
+INSERT INTO user_favourites (user_id, map_id) VALUES (1, 3);
+INSERT INTO user_favourites (user_id, map_id) VALUES (2, 4);
+INSERT INTO user_favourites (user_id, map_id) VALUES (2, 1);
+INSERT INTO user_favourites (user_id, map_id) VALUES (3, 3);
+INSERT INTO user_favourites (user_id, map_id) VALUES (3, 4);
+INSERT INTO user_favourites (user_id, map_id) VALUES (4, 3);
+INSERT INTO user_favourites (user_id, map_id) VALUES (4, 1);
+INSERT INTO user_favourites (user_id, map_id) VALUES (5, 2);
+INSERT INTO user_favourites (user_id, map_id) VALUES (5, 3);
