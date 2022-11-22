ea9e30c9ff5e797c1924215e9ef62fe2885bf88a
Will Wadman <wadmanwill@gmail.com>
2022-11-22 20:43:48 +0000

    add soem changes to eb resolved by master merge

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,6 +1,16 @@
+$(() => {
+function loginHelper(){
+  const userNumber = $("usernumber").val();
+  const loginForm = $('login_form').val();
+  let urlLink = "http://localhost:8080/users/login/"
+  urlLink = urlLink + userNumber;
+  loginForm.action = urlLink
+  console.log(urlLink)
+}
+});
+
 
 function loginHelper(){
   const action_src = "http://localhost:8080/users/login/" + document.getElementsByName("usernumber")[0].value;
-    let login_form = document.getElementById('login_form');
-    login_form.action = action_src ;
-}
+  let login_form = document.getElementById('login_form');
+  login_form.action = action_src ; }
