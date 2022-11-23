15a28ae76200807991e9373d85696091848f5791
doublefriedtofu <hurdonghyuk@gmail.com>
2022-11-23 18:47:39 -0500

    Nav favourite button linked Favourite text on the Nav bar redirects to maps/list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,7 @@
 // Client facing scripts here
+$(() => {
+  // redirect button to favourted and contirubted maps
+  $('.fav-link').on('click', function() {
+    window.location.href='/maps/list';
+  });
+});
